ef1159d1a1b764d927b0fb6d8b3593731b883eea
Ethan Chen <Ethanchen4@gmail.com>
2025-05-20 14:17:42 -0400

    added responsive menu for smaller screens

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "./mode-toggle";
 import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
+import Menu from "./menu";
 
 function Header() {
   return (
@@ -23,19 +21,7 @@ function Header() {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href={"/cart"}>
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
