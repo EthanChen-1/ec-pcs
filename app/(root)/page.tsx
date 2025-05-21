bdcc398504d6f6cdb486802b95d7044d68749475
Ethan Chen <Ethanchen4@gmail.com>
2025-05-21 17:57:52 -0400

    added a list of sample products and created product list and product card components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 function Homepage() {
-  return <>EC&apos;s Personal Computer Store</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 }
 
 export default Homepage;
